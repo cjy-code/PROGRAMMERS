8a639e8fd5bd25e2efa72a37759d5abf4a6b0463
jychoi <ggozxc@gmail.com>
2022-10-30 23:15:53 +0900

    Add Lessons132267   - title : 콜라 문제   - url : https://school.programmers.co.kr/learn/courses/30/lessons/132267?language=java

---
+++ src/com/porgrammers/src/Lessons132267.java
@@ -0,0 +1,20 @@
+package com.porgrammers.src;
+
+/**
+ * title: 콜라 문제
+ * url: https://school.programmers.co.kr/learn/courses/30/lessons/132267?language=java
+ * 최진영
+ */
+public class Lessons132267 {
+    static Lessons132267 solution = new Lessons132267();
+
+    public static void main(String[] args) {
+        solution.solution(2, 1, 20);
+    }
+
+    public int solution(int a, int b, int n) {
+        
+        int answer = 0;
+        return answer;
+    }
+}
