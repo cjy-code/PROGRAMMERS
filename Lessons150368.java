90f4599141cd471a7bc2e02e106f5a4d4ebdd979
cjy-code <ggozxc@gmail.com>
2023-01-16 20:41:42 +0900

    Fisrt Commit
    TITLE 2023 KAKAO BLIND RECRUITMENT 이모티콘 할인행사
    URL https://school.programmers.co.kr/learn/courses/30/lessons/150368
    SINCE 2023-01-16
    SCORE
    최진영

---
+++ Lessons150368.java
@@ -0,0 +1,24 @@
+package com.programmers.coding.test;
+
+/**
+ * TITLE 2023 KAKAO BLIND RECRUITMENT 이모티콘 할인행사
+ * URL https://school.programmers.co.kr/learn/courses/30/lessons/150368
+ * SINCE 2023-01-16
+ * SCORE
+ * 최진영
+ */
+public class Lessons150368 {
+
+    // Question 1
+    static int[][] users = {{40,10000}, {25,10000}};
+    static int[] emoticons = {7000, 9000};
+
+    public static void main(String[] args) {
+
+    }
+
+    public int[] solution(int[][] users, int[] emoticons) {
+        int[] answer = {};
+        return answer;
+    }
+}
